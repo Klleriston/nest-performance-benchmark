243eee061e334e1faa41d71cb53c2849d568f868
klleriston <klleandrade@outlook.com>
2025-03-16 18:18:54 -0300

    fix: error in engine

--- nest-fastify/src/main.ts
+++ nest-fastify/src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter(),
   );
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(3001, '0.0.0.0');
 }
 void bootstrap();
